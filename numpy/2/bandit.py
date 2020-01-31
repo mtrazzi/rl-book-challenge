d7f022d93d5c0f3179ec5fa9407447babdd8e0ed
mtrazzi <michael.trazzi@gmail.com>
2020-01-31 17:23:11 +0100

    added figures for ex2_5 and fig2_2

--- numpy/2/bandit.py
+++ numpy/2/bandit.py
@@ -5,7 +5,7 @@ class Bandit:
   def __init__(self, k=10, mean=0):
     self.k = k
     self.q = np.random.randn(k) + mean
-    self.max_action = max(self.q)
+    self.max_action = np.argmax(self.q)
 
   def reward(self, action):
     return np.random.normal(self.q[action])
--- numpy/2/figures.py
+++ numpy/2/figures.py
@@ -32,36 +32,45 @@ def a_simple_bandit_algorithm(bandit, n_iterations=1000, eps=0.1,
                               weight_fn=sample_average, random_walk=False,
                               Q_1=0, method='epsilon-greedy'):
   """Returns the estimated Q-Values of the bandit problem."""
-  k, q = bandit.k, bandit.q
+  k, q, max_action = bandit.k, bandit.q, bandit.max_action
   Q, N, R_log = np.zeros(k) + Q_1, np.zeros(k), np.zeros(k)
-  avg_rew, distance_to_true_q = [], []
-  for t in range(n_iterations):
+  avg_rew, distance_to_true_q, per_list = [], [], []
+  avg_r, per_max_act = 0, 0
+  for t in range(1, n_iterations + 1):
     A = action_selection(Q, eps, method=method, t=t, N=N)
     R = bandit.reward(A)
     N[A] += 1
     Q[A] += (R - Q[A]) * weight_fn(N[A])
     R_log[A] += (R-R_log[A]) * (1 / N[A])
-    avg_rew = average_reward(R_log, N)
-    avg_rew.append(avg_rew)
+    avg_r += (R - avg_r) / t
+    per_max_act += ((A == max_action) - per_max_act) / t
+    per_list.append(per_max_act)
+    avg_rew.append(avg_r)
     distance_to_true_q.append(np.linalg.norm(Q - q))
     if random_walk:
       q += 0.01 * np.random.randn(k)
-  return Q, N, avg_rew, distance_to_true_q
+  return Q, np.array(per_list), np.array(avg_rew), np.array(distance_to_true_q)
 
 
-def plot_average(arr, eps_list, n_bandits, y_lim, show=True, extra_label=''):
+def plot_average(arr, eps_list, n_bandits, y_lim, show=True, extra_label='',
+                 title=None, percentage=False):
   for i, eps in enumerate(eps_list):
-    plt.plot(arr[i][1:] / n_bandits, label=f"eps={eps} {extra_label}")
+    plt.plot((arr[i][1:] / n_bandits) * (100 if percentage else 1),
+             label=f"epsilon={eps} {extra_label}")
   axes = plt.gca()
   axes.set_ylim(y_lim)
+  plt.xlabel("Steps")
+  plt.ylabel("Optimal Action %" if percentage else "Average Reward")
   plt.legend()
+  if title is not None:
+    plt.title(title)
   if show:
     plt.show()
 
 
 def plot_figures(k, n_bandits, n_steps, eps_list, weight_fn, random_walk,
                  y_bounds_1, y_bounds_2=None, Q_1=0, norm=False, show=True,
-                 method='epsilon-greedy', extra_label=''):
+                 method='epsilon-greedy', extra_label='', title=None):
   avg_rew_per_eps = [np.zeros(n_steps) for _ in range(len(eps_list))]
   avg_rew_in_perc = [np.zeros(n_steps) for _ in range(len(eps_list))]
   distance_to_true_q = [np.zeros(n_steps) for _ in range(len(eps_list))]
@@ -69,21 +78,22 @@ def plot_figures(k, n_bandits, n_steps, eps_list, weight_fn, random_walk,
     print(i)
     bandit_pb = Bandit(k)
     for i, eps in enumerate(eps_list):
-      Q, _, avg_rew, d = a_simple_bandit_algorithm(bandit_pb,
-                                                   n_iterations=n_steps,
-                                                   eps=eps,
-                                                   weight_fn=weight_fn,
-                                                   random_walk=random_walk,
-                                                   Q_1=Q_1,
-                                                   method=method)
+      Q, per, avg_rew, d = a_simple_bandit_algorithm(bandit_pb,
+                                                     n_iterations=n_steps,
+                                                     eps=eps,
+                                                     weight_fn=weight_fn,
+                                                     random_walk=random_walk,
+                                                     Q_1=Q_1,
+                                                     method=method)
       avg_rew_per_eps[i] += avg_rew
-      avg_rew_in_perc[i] += (avg_rew / bandit_pb.max_action)
+      avg_rew_in_perc[i] += per
       distance_to_true_q[i] += d
   plot_average(avg_rew_per_eps, eps_list, n_bandits, y_bounds_1, show=show,
-               extra_label=extra_label)
+               extra_label=extra_label, title=title)
   if y_bounds_2 is not None:
-    plot_average(avg_rew_in_perc * 100, eps_list, n_bandits, y_bounds_2,
-                 show=show, extra_label=extra_label)
+    plot_average(avg_rew_in_perc, eps_list, n_bandits, y_bounds_2,
+                 show=show, extra_label=extra_label, title=title,
+                 percentage=True)
   if norm:
     plot_average(distance_to_true_q, eps_list, n_bandits, [0, 10], show=show,
                  extra_label=extra_label)
@@ -91,22 +101,27 @@ def plot_figures(k, n_bandits, n_steps, eps_list, weight_fn, random_walk,
 
 def main():
   # reproducing figure 2.2
-  plot_figures(10, 20, 1000, [0, 0.1, 0.01], sample_average, False, [0, 1.5],
-               [0, 1])
+  plot_figures(10, 2000, 1000, [0, 0.1, 0.01], sample_average, False, [0, 1.5],
+               [0, 100], title='Figure 2.2')
 
-  # # exercise 2.5
-  plot_figures(10, 5, 10000, [0.1], sample_average, True, [0, 2])
-  plot_figures(10, 5, 10000, [0.1], constant_alpha(alpha=0.1), True, [0, 3])
+  # exercise 2.5: difficulties of sample average on non-stationary problems
+  plot_figures(10, 100, 10000, [0.1], sample_average, True, [0, 2], show=False,
+               extra_label='sample average',
+               title="Exercise 2.5: sample-average vs.constant step size")
+  plot_figures(10, 100, 10000, [0.1], constant_alpha(alpha=0.1), True, [0, 3],
+               extra_label='constant step size (alpha = 0.1)')
 
   # figure 2.3: optimistic greedy vs. realistic eps-greedy
   plot_figures(10, 2000, 1000, [0], constant_alpha(alpha=0.1), False, [0, 3],
-               Q_1=5, show=False)
-  plot_figures(10, 2000, 1000, [0.1], constant_alpha(alpha=0.1), False, [0, 3])
+               Q_1=5, show=False, title="Figure 2.3", extra_label='Q_1=5')
+  plot_figures(10, 2000, 1000, [0.1], constant_alpha(alpha=0.1), False, [0, 3],
+               extra_label='Q_1=0')
 
   # reproducing figure 2.4
-  plot_figures(10, 20, 1000, [0.1], sample_average, True, [0, 2], show=False)
-  plot_figures(10, 20, 1000, [0.1], sample_average, True, [0, 2], method='ucb',
-               extra_label='ucb')
+  plot_figures(10, 200, 1000, [0.1], sample_average, False, [0, 1.5],
+               show=False)
+  plot_figures(10, 200, 1000, [0.1], sample_average, False, [0, 1.5],
+               method='ucb', extra_label='ucb')
 
 
 if __name__ == "__main__":
