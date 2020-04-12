075607d59dc8b547cb6ca87d793c956624ec242d
Michaël Trazzi <michael.trazzi@gmail.com>
2020-04-12 20:26:28 +0200

    example 6.2

--- numpy/6/figures.py
+++ numpy/6/figures.py
@@ -1,8 +1,14 @@
 import argparse
 from td import OneStepTD
 from driving import DrivingEnv, TRAVEL_TIME
+import numpy as np
 from randomwalk import RandomWalk
 import matplotlib.pyplot as plt
+
+N_EP_EX_6_2 = 100
+N_RUNS_EX_6_2 = 100
+TRUE_VALUES_EX_6_2 = [1/6, 2/6, 3/6, 4/6, 5/6]
+DEFAULT_FONT = {'fontsize': 14}
 
 def print_driving_home(states, V_old, V_new, fig, fig_id, ax_title):
   ax = fig.add_subplot(fig_id)
@@ -40,25 +46,25 @@ def fig_6_1():
   plt.show()
 
 def print_random_walk(ax, state_labels, td_vals):
-  ax.set_xticklabels(state_labels, fontdict={'fontsize': 8})
-  ax.set_xlabel('state')
-  ax.set_ylabel('estimated value') 
-  true_values = [(x + 1)/6 for x in range(len(state_labels))]
-  plt.plot(true_values, label='true values')
+  ax.set_xticklabels(state_labels, fontdict=DEFAULT_FONT)
+  x_ticks = np.arange(len(state_labels))
+  ax.set_xticks(x_ticks)
+  ax.set_xlabel('state', fontdict=DEFAULT_FONT)
+  ax.set_ylabel('estimated value', fontdict=DEFAULT_FONT) 
+  plt.plot(x_ticks, TRUE_VALUES_EX_6_2, label='true values')
   for key,td_val in td_vals.items():
-    plt.plot(td_val, label=key)
+    plt.plot(x_ticks, td_val[:-1], label=str(key) + ' episodes')
 
 def example_6_2():
   fig = plt.figure()
-  fig.suptitle('Example 6.2')
+  fig.suptitle('Example 6.2', fontdict=DEFAULT_FONT)
   env = RandomWalk()
   pi = {(a, s): 1.0 for s in env.states for a in env.moves} 
-  V_0 = [1/2 for s in env.states]
+  V_0 = [1/2 for s in env.states[:-1]] + [0]  # V = 0 for absorbing state
   V_init = {s: V_0[idx] for (idx, s) in enumerate(env.states)}
- 
+  
   alg = OneStepTD(env, V_init=V_init, step_size=0.1, gamma=1)
   tot_ep = 0
-  
   td_vals = {}
   ax = fig.add_subplot('121')
   for n_episodes in [0, 1, 10, 100]:
@@ -66,15 +72,42 @@ def example_6_2():
     td_vals[n_episodes] = alg.get_value_list()
   print_random_walk(ax, ["A", "B", "C", "D", "E"], td_vals)
   plt.legend()
+  
+  ax = fig.add_subplot('122')
+  td_step_sizes = [0.05, 0.1, 0.15]
+  td_vals = {alpha: [] for alpha in td_step_sizes}
+  mc_step_sizes = [0.01, 0.02, 0.03, 0.04]
+  mc_vals = {alpha: [] for alpha in mc_step_sizes}
+  runs_dict = {alpha: np.zeros(N_EP_EX_6_2) for alpha in td_step_sizes + mc_step_sizes} 
+  to_compare_list = [(td_step_sizes, alg.tabular_td_0, td_vals), (mc_step_sizes, alg.constant_step_size_mc, mc_vals)]
+  for (step_size_list, algorithm, store_dict) in to_compare_list:
+    for step_size in step_size_list:
+      alg.step_size = step_size
+      print(f"running step size {step_size}")
+      for seed in range(N_RUNS_EX_6_2): 
+        alg.reset()
+        alg.env.seed(seed)
+        err_l = []
+        for _ in range(N_EP_EX_6_2):
+          algorithm(pi, 1)
+          v_arr = np.array(alg.get_value_list()[:-1])
+          err_l.append(np.linalg.norm(v_arr-TRUE_VALUES_EX_6_2))
+        runs_dict[step_size] += np.array(err_l)
+
+  for key in runs_dict.keys():
+    runs_dict[key] /= N_RUNS_EX_6_2
+  
+  ax.set_xlabel('walks / episodes', fontdict=DEFAULT_FONT)
+  ax.set_ylabel('empirical rms error averaged over states', fontdict=DEFAULT_FONT) 
+  for key,err_run in runs_dict.items():
+    (color, alg_name) = ('b','td') if key in td_step_sizes else ('r', 'mc')
+    linewidth = int(100 * key) / 10 if key in td_step_sizes else int(200 * key) / 10
+    linestyle = 'dashed' if key in [0.02, 0.03] else None
+    plt.plot(err_run, color=color, label=alg_name + ' (a=' + str(key) + ')', linewidth=linewidth, linestyle=linestyle)
+   
+  plt.legend()
   plt.savefig('example6.2.png')
   plt.show()
- 
-  ## constant step size mc
-  #alg.reset()
-  #alg.constant_step_size_mc(pi)
-  #V_MC = alg.get_value_list()
-  #print_driving_home(env.states, V_0, V_MC, fig, '122', 'constant step size mc')
-
 
 
 PLOT_FUNCTION = {
--- numpy/6/randomwalk.py
+++ numpy/6/randomwalk.py
@@ -2,9 +2,10 @@ import numpy as np
 
 N_STATES = 5
 EMPTY_MOVE = 0
-STATES = np.arange(N_STATES)
+STATES = list(range(N_STATES))
 P_LEFT = 0.5
 R_STEP = 0
+ABSORBING_STATE = N_STATES
 
 class RandomWalk:
   def __init__(self):
@@ -16,7 +17,7 @@ class RandomWalk:
 
   @property
   def states(self):
-    return STATES
+    return STATES + [ABSORBING_STATE]
 
   def sample_shift(self):
     return np.sign(np.random.random() - P_LEFT)
@@ -24,13 +25,16 @@ class RandomWalk:
   def step(self, action):
     new_state = self.state + self.sample_shift() 
     if not (0 <= new_state < N_STATES):
-      return self.state, new_state == N_STATES, True, {}
+      return ABSORBING_STATE, float(new_state == N_STATES), True, {}
     self.state = new_state
     return self.state, R_STEP, False, {}
 
   def reset(self):
     self.state = N_STATES // 2
     return self.state
+
+  def seed(self, seed):
+    np.random.seed(seed)
 
   def __str__(self):
     return self.state
