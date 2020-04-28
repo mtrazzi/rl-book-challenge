a891a5f1691b59ebc5b515735796271cab1a3216
Michaël Trazzi <michael.trazzi@gmail.com>
2020-04-28 16:00:36 +0200

    7.2 n=1 works

--- chapter7/figures.py
+++ chapter7/figures.py
@@ -7,38 +7,16 @@ from randomwalk import RandomWalk, EMPTY_MOVE
 UND = 1
 FIG_7_2_N_EP = 10
 FIG_7_2_N_STATES = 19
-FIG_7_2_N_RUNS = 100
-
-def true_values(n_states):
-  return np.linspace(-1, 1, n_states + 2)[1:-1]
-
-def comp_true_vals(n_states, n_ep_per_s=10000):
-  env = RandomWalk(n_states)
-  vals = []
-  for state in range(n_states + 1):
-    print(state)
-    tot = 0
-    for _ in range(n_ep_per_s):
-      r_ep, d = 0, False
-      env.force_state(state)
-      while not d:
-        _, r, d, _ = env.step(EMPTY_MOVE)
-        r_ep += r
-      tot += r_ep
-    vals.append(tot / n_ep_per_s)
-  return np.array(vals[:-1])
-
-def ex_7_1():
-  pass
+FIG_7_2_N_RUNS = 10
 
 def fig_7_2():
   fig, ax = plt.subplots()
   ax.set_title('Figure 7.2')
-  n_l = [1, 2, 3]
-  alpha_l = np.linspace(0, 1, 11)[1:]
+  n_l = [1]
+  alpha_l = np.linspace(0, 1, 11)
   env = RandomWalk(n_states=FIG_7_2_N_STATES)
   pi = {(a, s): 1.0 for s in env.states for a in env.moves_d[s]}
-  true_vals = true_values(env.n_states)
+  true_vals = np.linspace(-1, 1, env.n_states + 2)[1:-1] 
   alg = nStepTD(env, V_init=None, step_size=None, gamma=UND, n=None)
   for n in n_l:
     alg.n = n
@@ -51,11 +29,11 @@ def fig_7_2():
       for seed in range(FIG_7_2_N_RUNS):
         alg.reset()
         alg.seed(seed)
-        alg.pol_eval(pi, n_ep=FIG_7_2_N_EP)
-        #alg.simple_td(pi, n_ep=FIG_7_2_N_EP)
-        v_arr = np.array(alg.get_value_list()[:-1])
-        err_sum += np.linalg.norm(v_arr-true_vals)
-      err_l.append(err_sum / FIG_7_2_N_RUNS)
+        for _ in range(FIG_7_2_N_EP):
+          alg.pol_eval(pi, n_ep=1)
+          v_arr = np.array(alg.get_value_list()[:-1])
+          err_sum += np.sqrt(np.sum((v_arr-true_vals) ** 2) / FIG_7_2_N_STATES)
+      err_l.append(err_sum / (FIG_7_2_N_RUNS * FIG_7_2_N_EP))
     plt.plot(alpha_l, err_l, label=f'n={n}')
   ax.set_xticks(np.linspace(0, 1, 6))
   ax.set_xlabel('Stepsize')
@@ -64,7 +42,6 @@ def fig_7_2():
   plt.show()
 
 PLOT_FUNCTION = {
-  'ex7.1': ex_7_1,
   '7.2': fig_7_2,
 }
 
