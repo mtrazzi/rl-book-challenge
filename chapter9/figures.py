159c8a3920ef30adf4f6a515ad449b409745bfb6
Michaël Trazzi <michael.trazzi@gmail.com>
2020-05-26 13:41:52 +0200

    nstep state aggregation compiles

--- chapter9/figures.py
+++ chapter9/figures.py
@@ -2,23 +2,24 @@ import argparse
 import matplotlib.pyplot as plt
 from randomwalk import RandomWalk, EMPTY_MOVE
 from gradient_methods import GradientMC, SemiGradientTD0
+from nstep_semi_grad import nStepSemiGrad
 from utils import est
 import numpy as np
 
 MED_FONT = 13
 
-EXA_9_1_ALP = 2e-5
-EXA_9_1_W_DIM = 100
-EXA_9_1_N_EP = 10 ** 5
-EXA_9_1_N_EP_TR = 1000
-EXA_9_1_G = 1
+FIG_9_1_ALP = 2e-5
+FIG_9_1_W_DIM = 100
+FIG_9_1_N_EP = 10 ** 5
+FIG_9_1_N_EP_TR = 1000
+FIG_9_1_G = 1
 
-EXA_9_2_ALP = EXA_9_1_ALP
-EXA_9_2_W_DIM = EXA_9_1_W_DIM
-EXA_9_2_N_EP = 10 ** 2
-EXA_9_2_N_EP_TR = 1000
-EXA_9_2_G = 1
-
+FIG_9_2_ALP = FIG_9_1_ALP
+FIG_9_2_W_DIM = FIG_9_1_W_DIM
+FIG_9_2_N_EP = int(5e2)
+FIG_9_2_N_EP_TR = 10
+FIG_9_2_G = 1
+FIG_9_2_N = 2
 
 def save_plot(filename, dpi=None):
   plt.savefig('plots/' + filename + '.png', dpi=dpi)
@@ -47,11 +48,11 @@ def nab_vhat_st_agg(s, w):
 def fig_9_1():
   env = RandomWalk() 
   pi = {(EMPTY_MOVE, s): 1 for s in env.states}
-  grad_mc = GradientMC(env, EXA_9_1_ALP, EXA_9_1_W_DIM)
+  grad_mc = GradientMC(env, FIG_9_1_ALP, FIG_9_1_W_DIM)
   grad_mc.seed(0)
-  grad_mc.pol_eva(pi, vhat_st_agg, nab_vhat_st_agg, EXA_9_1_N_EP, EXA_9_1_G)
+  grad_mc.pol_eva(pi, vhat_st_agg, nab_vhat_st_agg, FIG_9_1_N_EP, FIG_9_1_G)
   est_vals = [vhat_st_agg(s, grad_mc.w) for s in env.states][:-1]
-  true_vals = [est(env, pi, s, EXA_9_1_G, n_ep=EXA_9_1_N_EP_TR) for s in env.states]
+  true_vals = [est(env, pi, s, FIG_9_1_G, n_ep=FIG_9_1_N_EP_TR) for s in env.states]
 
   fig, ax1 = plt.subplots()
   ax1.plot(est_vals, 'b', label='Approximate MC value vhat')
@@ -67,13 +68,24 @@ def fig_9_1():
   plt.show()
 
 def fig_9_2():
+  fig = plt.figure()
   env = RandomWalk()
   pi = {(EMPTY_MOVE, s): 1 for s in env.states}
-  semi_grad_td = SemiGradientTD0(env, EXA_9_1_ALP, EXA_9_1_W_DIM)
+
+  semi_grad_td = SemiGradientTD0(env, FIG_9_2_ALP, FIG_9_2_W_DIM)
   semi_grad_td.seed(0)
-  semi_grad_td.pol_eva(pi, vhat_st_agg, nab_vhat_st_agg, EXA_9_1_N_EP, EXA_9_1_G)
-  est_vals = [vhat_st_agg(s, semi_grad_td.w) for s in env.states][:-1]
-  true_vals = [est(env, pi, s, EXA_9_1_G, n_ep=EXA_9_1_N_EP_TR) for s in env.states]
+  #semi_grad_td.pol_eva(pi, vhat_st_agg, nab_vhat_st_agg, FIG_9_2_N_EP, FIG_9_2_G)
+  #est_vals = [vhat_st_agg(s, semi_grad_td.w) for s in env.states][:-1]
+  #ax1 = fig.add_subplot('211')
+  ##true_vals = [est(env, pi, s, FIG_9_2_G, n_ep=FIG_9_2_N_EP_TR) for s in env.states]
+  #ax1.plot(est_vals, 'b', label='Approximate MC value vhat')
+  ##ax1.plot(true_vals, 'r', label='True value v_pi')
+  #plot_figure(ax1, 'Figure 9.2', [0, 999], [1, 1000], 'State', [-1, 0, 1], [-1, 0, 1], '\n\nValue\nScale')
+
+  nstep_semi_grad = nStepSemiGrad(env, FIG_9_2_ALP, FIG_9_2_W_DIM, FIG_9_2_G, FIG_9_2_N)
+  nstep_semi_grad.pol_eval(pi, vhat_st_agg, nab_vhat_st_agg, FIG_9_2_N_EP)
+  save_plot('fig9.2', dpi=100)
+  plt.show()
 
 PLOT_FUNCTION = {
   '9.1': fig_9_1,
---
+++ chapter9/nstep_semi_grad.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+class nStepSemiGrad:
+  def __init__(self, env, alpha, w_dim, gamma, n):
+    self.env = env
+    self.a = alpha
+    self.n = n
+    self.d = w_dim
+    self.g = gamma
+    self.reset()
+
+  def sample_action(self, pi, s):
+    pi_dist = [pi[(a, s)] for a in self.env.moves_d[s]]
+    return self.env.moves_d[s][np.random.choice(np.arange(len(self.env.moves_d[s])), p=pi_dist)]
+
+  def get_r_values(self, R, i, j):
+    n = self.n
+    orig_mod = mod_idx = i % (n + 1)
+    goal = j % (n + 1)
+    R_vals = []
+    while True:
+      R_vals.append(R[mod_idx])
+      mod_idx = (mod_idx + 1) % (n + 1)
+      if mod_idx == goal:
+        return R_vals
+
+  def n_step_return(self, vhat, tau, T):
+    S, R, n, g_l = self.S, self.R, self.n, self.g_l
+    max_idx = min(tau + n, T)
+    r_vals = self.get_r_values(R, tau + 1, max_idx + 1)
+    G = np.dot(g_l[:max_idx-tau], r_vals)
+    if tau + n < T:
+      G = G + g_l[n] * vhat(S[(tau + n) % (n + 1)], self.w)
+    return G
+
+  def pol_eval(self, pi, vhat, nab_vhat, n_ep):
+    n, R, S = self.n, self.R, self.S
+    for ep in range(n_ep):
+      print(ep)
+      S[0] = self.env.reset()
+      T = np.inf
+      t = 0
+      while True:
+        if t < T:
+          S[(t + 1) % (n + 1)], R[(t + 1) % (n + 1)], d, _ = self.env.step(self.sample_action(pi, S[t % (n + 1)]))
+          if d:
+            T = t + 1
+        tau = t - n + 1
+        if tau >= 0:
+          s = S[tau % (n + 1)]
+          G = self.n_step_return(vhat, tau, T)
+          self.w += self.a * (G - vhat(s, self.w)) * nab_vhat(s, self.w)
+        if tau == (T - 1):
+          break
+        t += 1
+
+  def seed(self, seed):
+    self.env.seed(seed)
+    np.random.seed(seed)
+
+  def reset(self):
+    self.g_l = [self.g ** k for k in range(self.n + 1)]
+    self.S = [None for _ in range(self.n + 1)]
+    self.R = [None for _ in range(self.n + 1)]
+    self.w = np.zeros(self.d)
